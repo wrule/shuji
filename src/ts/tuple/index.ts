b80c3b9114b02a71dd7d75fb8c9fab5dc4224d8c
jimao <1982775886@qq.com>
2020-09-28 22:42:23 +0800

    tuple contain初步完成

--- src/ts/tuple/index.ts
+++ src/ts/tuple/index.ts
@@ -13,4 +13,28 @@ export class TsTuple extends TS {
   public Compare(ts: TS): number {
     return 0;
   }
+
+  public Contain(ts: TS): boolean {
+    if (ts.Type === TsType.Tuple) {
+      const tuple = ts as TsTuple;
+      if (this.Members.length >= tuple.Members.length) {
+        return tuple.Members
+          .every((member, index) => this.Members[index].Contain(member));
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  private get Members() {
+    return this.members;
+  }
+
+  public constructor(
+    private members: TS[],
+  ) {
+    super();
+  }
 }
