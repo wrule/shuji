930bbf140586e32c73ced87e957ed19370ffb9c8
jimao <1982775886@qq.com>
2020-09-28 22:59:27 +0800

    object contaion

--- src/ts/object/index.ts
+++ src/ts/object/index.ts
@@ -13,4 +13,32 @@ export class TsObject extends TS {
   public Compare(ts: TS): number {
     return 0;
   }
+
+  public Contain(ts: TS): boolean {
+    if (ts.Type === TsType.Object) {
+      const object = ts as TsObject;
+      return Array.from(object.Fields).every((ary) => {
+        const key = ary[0];
+        const dstType = ary[1];
+        const srcType = this.Fields.get(key);
+        if (srcType) {
+          return srcType.Contain(dstType);
+        } else {
+          return false;
+        }
+      });
+    } else {
+      return false;
+    }
+  }
+
+  public get Fields() {
+    return this.fields;
+  }
+
+  public constructor(
+    private fields: Map<string, TS>,
+  ) {
+    super();
+  }
 }
