0260f919441b4ebf39d4455038c384c3eefceb4a
jimao <1982775886@qq.com>
2020-10-03 01:51:59 +0800

    修改测试代码

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,26 @@ import { Infer } from './ts/index';
 import { JsField } from './js/field';
 import { StructObject } from './struct/object';
 
-const object = {
-  name: 'gushi',
-  age: 27,
-  sex: true,
-  address: '浙江省杭州市',
-  meta: {
-    like: ['家人', '健康', '美女'],
-    data: [
-      ['身高', 186],
-      ['体重', 220],
-      ['智商', 120],
-    ],
-  },
-};
+// const object = {
+//   name: 'gushi',
+//   age: 27,
+//   sex: true,
+//   address: '浙江省杭州市',
+//   meta: {
+//     like: ['家人', '健康', '美女'],
+//     data: [
+//       ['身高', 186],
+//       ['体重', 220],
+//       ['智商', 120],
+//     ],
+//   },
+// };
+
+const object = [1, 2, '', '', '', '', ''];
 
 const jsField = new JsField('me', object);
 
 const struct = Infer(jsField.Value);
 
-console.log( ((struct as StructObject).Fields.get('meta') as StructObject) );
+console.log(struct);
 
