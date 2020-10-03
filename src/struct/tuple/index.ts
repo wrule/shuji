5d271ed407a6239c4b925cc1fb732bf2d7ab4ea2
jimao <1982775886@qq.com>
2020-10-04 00:47:29 +0800

    删除注释

--- src/struct/tuple/index.ts
+++ src/struct/tuple/index.ts
@@ -53,7 +53,6 @@ export class StructTuple extends Struct {
     }
   }
 
-  // TODO Array能不能和Tuple合并
   protected iMerge(ts: Struct): Struct {
     if (ts.Type === this.Type) {
       const tuple = ts as StructTuple;
