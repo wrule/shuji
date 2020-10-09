52c4fd2d150d920d40cd853b54d73e532e8252a2
hejing <wei.zhang@perfma.com>
2020-10-09 18:15:35 +0800

    包含逻辑导致对象字段产生联合失效

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Infer } from './infer/index';
 import { JsField } from './js/field';
 import { StructObject } from './struct/object';
-import object from './test/index2.json';
+import object from './test/index3.json';
 import API from './namespace/api';
 import { IResponse, Response } from './namespace/api2';
 import { StructArray } from './struct/array';
@@ -31,8 +31,8 @@ const jsField = new JsField('me', object);
 
 const oldTime = Number(new Date());
 const struct = Infer(jsField.Value, 'rsp');
-const tss = struct as StructObject;
+const tss = struct as StructArray;
 console.log('耗时', Number(new Date()) - oldTime);
 console.log(struct.Hash);
-console.log(((tss.Fields.get('object') as StructArray).ElementStruct as StructObject).TsDef('gu'));
+console.log(((tss as StructArray).ElementStruct as StructObject).TsDef('gu'));
 console.log(tss.SpaceObjects.length);
