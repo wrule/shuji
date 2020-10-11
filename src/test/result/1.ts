1d4a49ce0d50179b8798ceac87bf647abfdccdaa
jimao <1982775886@qq.com>
2020-10-11 11:30:13 +0800

    暂时不外部插件化

--- src/test/result/1.ts
+++ src/test/result/1.ts
@@ -21,9 +21,35 @@ export module Rsp {
 }
 
 export interface Call {
-  
+
 }
 
 export default function Call(req: any): IRsp {
   return {} as any;
+}
+
+import axios from 'axios';
+
+
+
+axios.post('', {}, { params: {} });
+
+axios.put('', {}, { params: {} });
+
+axios.patch('', {}, { params: {} });
+
+axios.get('', { params: {} });
+
+axios.options('', { params: {} });
+
+axios.head('', { params: {} })
+
+axios.delete('', { params: {} })
+
+function a(params: any, query: any) {
+
+}
+
+function b(query: any) {
+
 }
