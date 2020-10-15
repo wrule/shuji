b42a3e65b0ede0860cd8324167eca068fcf552fc
jimao <1982775886@qq.com>
2020-10-15 18:27:22 +0800

    非继承的实现

--- src/utils/syncRedis/index.ts
+++ src/utils/syncRedis/index.ts
@@ -10,6 +10,9 @@ class SyncRedis {
     return this.syncGet(key);
   }
 
+
+  Set(key: KeyType, value: Redis.ValueType, callback: Redis.Callback<Redis.Ok>): void;
+  Set(key: KeyType, value: Redis.ValueType, setMode: string | any[], callback: Redis.Callback<Redis.Ok | null>): void;
   public Set(key: Redis.KeyType, value: Redis.ValueType): 'OK' | null {
     return this.syncSet(key, value);
   }
