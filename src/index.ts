59d285a1d1078bcd37752ed57f2e2d5a1560cb9c
jimao <1982775886@qq.com>
2020-10-17 22:05:23 +0800

    出口模块,测试代码

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,9 @@
 import { Infer } from './infer/index';
 import { JsField } from './js/field';
-import object from './test/index1.json';
-import fs from 'fs';
 
-// import { mainModule } from "process";
-
-const jsField = new JsField('me', object);
-const oldTime = Number(new Date());
-const struct = Infer(jsField.Value, 'rsp');
-console.log('耗时', Number(new Date()) - oldTime);
-fs.writeFileSync('output/1.ts', struct.TsTestCode, 'utf8');
-
-// import deasync from 'deasync';
-// var cp = require('child_process');
-
-// function asyncFunc(cb: (err: any, res: any) => void) {
-//   setTimeout(() => {
-//     cb(null, '同步');
-//   }, 2000);
-// }
-
-// const syncFunc = deasync(asyncFunc);
-
-// async function main() {
-//   console.log('start');
-//   const result = syncFunc();
-//   console.log(result);
-// }
-
-// main();
-
-// import { SyncRedis } from './utils/syncRedis';
-
-// const syncRedis = new SyncRedis();
-// syncRedis.SyncSet('nihao', '世界');
-// console.log(syncRedis.SyncGet('nihao'));
+export default class Shuji {
+  public Infer(name: string, value: any) {
+    const jsField = new JsField(name, value);
+    return Infer(jsField.Value, jsField.Name);
+  }
+}
